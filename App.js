7258bbfabac92c27078e59f9c8427cbe08d84d90
GauravKamdar <167879421+GauravKamdar@users.noreply.github.com>
2025-02-02 15:51:43 +0530

    Update App.js

--- App.js
+++ App.js
@@ -4,19 +4,21 @@ import Register from "./components/Register";
 import Login from "./components/Login";
 import Home from "./components/Home";
 import AddEditContact from "./components/AddEditContact";
+//import EditContact from "./components/EditContact";
 
-function App() {
+const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Register />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/Login" element={<Login />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/contacts" element={<Home />} />
         <Route path="/add-contact" element={<AddEditContact />} />
         <Route path="/edit-contact/:id" element={<AddEditContact />} />
       </Routes>
     </Router>
   );
-}
+};
 
 export default App;
